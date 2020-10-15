3818956b6fdbf264dd3f69f5cde5bf9856c30aeb
Oleg <mac@opawwce.local>
2020-10-15 15:35:54 +0500

    Create adding todos to the list

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import TodoList from "./components/TodoList";
 
 function App() {
   const [inputText, setInputText] = useState("");
-  const [todos, setTodos] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form setInputText={setInputText} />
-      <TodoList />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} />
     </div>
   );
 }
